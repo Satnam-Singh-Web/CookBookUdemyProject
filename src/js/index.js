653bb584e9d99523f3589d9841515ad1f24cc275
Satnam1313 <satnamsingh13135@gmail.com>
2019-02-06 12:54:25 -0400

    updated babel loader

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,4 @@
 // Global app controller
 import num from './test';
-console.log(`I impotrted ${num} from module`);
+const x = 23;
+console.log(`I impotrted ${num} from test.js! Variable x is${x}`);
